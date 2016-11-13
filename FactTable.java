9d36ad13d030913fada24d1556c1beddd369f057
joseantonioja <jose_antonio_ja97@hotmail.com>
2016-11-12 19:02:50 -0600

    Update FactTable.java
    Clase a ejecutar que tiene los métodos necesarios para obtener entropias y ganancias de las variables.

--- FactTable.java
+++ FactTable.java
@@ -2,17 +2,18 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 class FactTable{
-	private ArrayList< ArrayList<Integer> > attrOcurrences;
-	private ArrayList< ArrayList<String> > attrInstances;
-	private ArrayList< ArrayList<Double> > branchEntropies;
-	private String headers[];
-	private double entropies[];
-	private double gains[];
-	private int facts[][];
-	private int nRows;
-	private int nFilters;
-	private int generalAttribute;
-	private int particularOcurrences[][][];
+	private ArrayList< ArrayList<Integer> > attrOcurrences;//Almacena las ocurrencias de la j-esima instancia del i-esimo atributo
+	private ArrayList< ArrayList<String> > attrInstances;//Almacena la j-esima instancia del i-esimo atributo
+	private ArrayList< ArrayList<Double> > branchEntropies;//Almacena las entropias de cada nodo
+	private String headers[];//Almacena los nombres de los atributos a considerar
+	private double entropies[];//Almacena la entropia general de cada atributo o header
+	private double gains[];//Almacena la ganancia general de cada atributo o header
+	private int facts[][];//Almacena la tabla mapeada con indices que identifican a cada instancia
+	private int nRows;//Numero de hechos dentro de la tabla
+	private int nFilters;//Numero de instancias de la variable que se va a estudiar
+	private int generalAttribute;//Identificador del atributo que se estudiara
+	private int particularOcurrences[][][];//Tabla de frequencias respecto al atributo vs el atributo que se estudiara
+	/*Constructor de la clase. Se le pasan el numero de atributos y el numero de filas de la tabla*/
 	public FactTable(int nAtributes, int nRows){
 		facts = new int[nRows][nAtributes];
 		entropies = new double[nAtributes];
@@ -22,6 +23,7 @@ class FactTable{
 		attrInstances = new ArrayList();
 		branchEntropies = new ArrayList();
 	}
+	/*Pone los nombres de los atributos dentro de la tabla*/
 	public void setAttributes(String headers[]){
 		this.headers = headers;
 		for(int i=0; i<headers.length; i++){
@@ -31,6 +33,7 @@ class FactTable{
 			attrInstances.add(tmpS);
 		}
 	}
+	/*Agrega la i-esima fila a la tabla facts mapeando los strings de las instancias de atributos por identificadores enteros*/
 	public void addFact(int i, String attributes[]){
 		for(int j=0; j<attributes.length; j++){
 			int found = attrInstances.get(j).indexOf(attributes[j]);
@@ -45,6 +48,7 @@ class FactTable{
 			}
 		}
 	}
+	/*Imprime la tabla de hechos mapeada en Strings*/
 	public void impFact(){
 		for(int j=0; j<headers.length; j++){
 			System.out.print(headers[j] + "\t");
@@ -57,6 +61,7 @@ class FactTable{
 			System.out.println("");
 		}
 	}
+	/*Imprime las ocurrencias de las instancias de cada atributo o header*/
 	public void impOcurrences(){
 		for(int i=0; i<headers.length; i++){
 			System.out.println(headers[i]+":");
@@ -65,6 +70,7 @@ class FactTable{
 			}
 		}
 	}
+	/*Imprime la tabla de frequencias del header vs la variable a considerar*/
 	public void impFrequencyTables(){
 		System.out.println("Ocurrencias generales de "+headers[generalAttribute]);
 		for(int i=0; i<nFilters; i++){
@@ -89,6 +95,7 @@ class FactTable{
 			}	
 		}
 	}
+	/*Llena la tabla de frequencias del header vs la variable a considerar que se identifica por column*/
 	public void findFrequencies(int column){
 		generalAttribute = column;
 		nFilters = attrInstances.get(column).size();
@@ -105,10 +112,12 @@ class FactTable{
 			}
 		}		
 	}
+	/*Determina todas las entropias necesarias*/
 	public void findEntropies(){
 		findGeneralEntropy();
 		findParticularEntropies();
 	}
+	/*Encuentra la entropia del atributo que se va a estudiar*/
 	private void findGeneralEntropy(){
 		double prob, entropy = 0;
 		for(int i=0; i<attrOcurrences.get(generalAttribute).size(); i++){
@@ -117,6 +126,7 @@ class FactTable{
 		}
 		entropies[generalAttribute] = entropy;
 	}
+	/*Encuentra las entropias de los headers vs la variable a estudiar*/
 	private void findParticularEntropies(){
 		double tmp;
 		System.out.println("Branch entropies: ");
@@ -136,6 +146,7 @@ class FactTable{
 			}
 		}
 	}
+	/*Determina la entropia de una serie de numeros*/
 	public double entropy( int frequencies[], double total){
 		double prob, ent=0;
 		for(int i=0; i<nFilters; i++){
@@ -146,6 +157,7 @@ class FactTable{
 		}
 		return ent;
 	}	
+	/*Determina la ganancia de la informacion por cada header o atributo*/
 	public void findGains(){
 		System.out.println("Gains:");
 		for(int i=0; i<headers.length; i++){
@@ -155,14 +167,11 @@ class FactTable{
 			}
 		}
 	}
-	public static void impAttributes(String[] attr){
-		for(int i=0; i<attr.length; i++)
-			System.out.print(attr[i] + "\t");
-		System.out.println("");
-	}
+	/*Determina el logaritmo base 2 de p*/
 	private double log2(double p){
 		return Math.log(p)/Math.log(2);
 	}
+	/*Regresa el arbol generado por el algoritmo ID3 respecto a la variable a considerar identificada por column*/
 	public Tree id3(int column){
 		impFact();
 		impOcurrences();
