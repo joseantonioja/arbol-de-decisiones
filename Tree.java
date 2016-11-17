6f5eb0c41a5fe804e6c103758decb6445cba331e
joseantonioja <jose_antonio_ja97@hotmail.com>
2016-11-17 16:46:45 -0600

    Generador de arboles v2
    Tiene una correción para evitar sobreescritura de las ramas de los atributos cuando compartan instancias (por ejemplo, dos atributos de tipo booleano) asignandoles identificadores.

--- Tree.java
+++ Tree.java
@@ -1,20 +1,45 @@
 import java.util.*;
 public class Tree{
-	public HashMap<String, ArrayList<String> > adjList;
+	public ArrayList<ArrayList<Integer> > adjList;
+	public ArrayList<String> labels;
+	public HashMap<String, Integer> labelAttributes;
 	public String root;
+	public int counter;
 	public Tree(){
-		adjList = new HashMap();
-		root = null;
+		adjList = new ArrayList<ArrayList<Integer>>();
+		labels = new ArrayList<String>();
+		labelAttributes = new HashMap<String, Integer>();
+		counter = 0;
 	}
-	public void addNode(String u){
-		ArrayList<String> adjNodes = new ArrayList<String>();
-		adjList.put(u, adjNodes);
+	public int addNode(String u){
+		adjList.add(new ArrayList<Integer>());
+		labels.add(u);
+		return counter++;
 	}
-	public void addEdge(String u, String v){
-		ArrayList<String> tmp = (ArrayList)adjList.get(u);
-		tmp.add(v);
+	public int addAttribute(String attr){
+		int c = addNode(attr);
+		labelAttributes.put(attr, c);
+		return c;
+	}
+	public void addEdge(int u, int v){
+		adjList.get(u).add(v);
 	}
 	public String toString(){
-		return "Root:" + root + " "+adjList.toString(); 
+		String r = "{";
+		int n;
+		for(int i=0; i<adjList.size(); i++){
+			n = adjList.get(i).size();
+			if(n!=0){
+				r+=labels.get(i)+"=[";
+				for(int j=0; j<n-1; j++)
+					r+=labels.get(adjList.get(i).get(j)) + ", ";
+				r+=labels.get(adjList.get(i).get(n-1));
+				r+="]";
+				if(i<adjList.size()-2)
+					r+=", ";
+			}
+		}
+		r+="}";
+		return "Root:" + root + " " +r +" " +labelAttributes; 
 	}
 }
